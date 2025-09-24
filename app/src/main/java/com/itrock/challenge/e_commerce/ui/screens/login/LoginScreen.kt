67215f88bd9a0e0bc979f661e117f4ae9973c09e
Mauri <mauricio.caricari@gmail.com>
2025-09-24 11:02:59 -0300

    Deleted import

--- app/src/main/java/com/itrock/challenge/e_commerce/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/itrock/challenge/e_commerce/ui/screens/login/LoginScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
